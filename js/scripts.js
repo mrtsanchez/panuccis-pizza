df32cf1f89ef3c052c4223338f7e8429719fab49
Marta Sanchez <sanchez.marta@gmail.com>
2016-08-29 11:52:40 -0700

    add a readme file with specs

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,9 @@ function Checkout (){
   this.totalPrice = 0;
 };
 
-function Order (size, croust, toppings){
+function Order (size, crust, toppings){
   this.pizzaSize = size;
-  this.pizzaCroust = croust;
+  this.pizzaCrust = crust;
   this.pizzaToppings = toppings;
   this.delivery = true;
   this.price = 0;
@@ -19,7 +19,7 @@ function Contact (name, phone, street, city){
 };
 
 Order.prototype.orderDetails = function() {
-  return "1 Pizza, " + newPizzaOrder.pizzaSize + " size, with your choice of " + newPizzaOrder.pizzaCroust + " Croust, and " + newPizzaOrder.pizzaToppings.length + " toppings. Price: $" + newPizzaOrder.price + ".";
+  return "1 Pizza, " + newPizzaOrder.pizzaSize + " size, with your choice of " + newPizzaOrder.pizzaCrust + " crust, and " + newPizzaOrder.pizzaToppings.length + " toppings. Price: $" + newPizzaOrder.price + ".";
 };
 
 Contact.prototype.orderDelivery = function(){
@@ -31,9 +31,9 @@ var sizes = {
   pizzaSizePrices: [10, 8, 6]
 };
 
-var crousts = {
-  pizzaAvailableCroust: ["Crispy", "Stuffed", "Gluten-Free"],
-  croustPrices: [2, 3, 4]
+var crusts = {
+  pizzaAvailablecrust: ["Crispy", "Stuffed", "Gluten-Free"],
+  crustPrices: [2, 3, 4]
 };
 
 var toppings = {
@@ -41,9 +41,9 @@ var toppings = {
   toppingsPrices: [1, 1, 3, 2, 500, 1, 3, 1]
 };
 
-function addPizzatoOrder(size, croust, topping){
+function addPizzatoOrder(size, crust, topping){
 
-  newPizzaOrder = new Order (size, croust, topping);
+  newPizzaOrder = new Order (size, crust, topping);
   calculatePrice();
   finalOrder.totalPrice += newPizzaOrder.price;
   finalOrder.pizzas.push(newPizzaOrder);
@@ -52,7 +52,7 @@ function addPizzatoOrder(size, croust, topping){
 
 function calculatePrice(){
 
-  newPizzaOrder.price +=  sizes.pizzaSizePrices[sizes.pizzaAvailableSizes.indexOf(newPizzaOrder.pizzaSize)] + crousts.croustPrices[crousts.pizzaAvailableCroust.indexOf(newPizzaOrder.pizzaCroust)]
+  newPizzaOrder.price +=  sizes.pizzaSizePrices[sizes.pizzaAvailableSizes.indexOf(newPizzaOrder.pizzaSize)] + crusts.crustPrices[crusts.pizzaAvailablecrust.indexOf(newPizzaOrder.pizzaCrust)]
 
   for (var i = 0; i < newPizzaOrder.pizzaToppings.length; i++) {
   var toppingPrice = toppings.toppingsNames.indexOf(newPizzaOrder.pizzaToppings[i]);
@@ -64,7 +64,7 @@ function calculatePrice(){
 function resetForm(){
 
   $("input:radio[name=pizza-size]").prop('checked', false);
-  $("input:radio[name=pizza-croust]").prop('checked', false);
+  $("input:radio[name=pizza-crust]").prop('checked', false);
   $("input:checkbox[name=toppings]").prop('checked', false);
 
 };
@@ -86,7 +86,7 @@ $(document).ready(function() {
 
     var newPizzaSize = $("input:radio[name=pizza-size]:checked").val();
 
-    var newPizzaCroust = $("input:radio[name=pizza-croust]:checked").val();
+    var newpizzaCrust = $("input:radio[name=pizza-crust]:checked").val();
 
     var newPizzaToppings = [];
 
@@ -94,16 +94,14 @@ $(document).ready(function() {
       newPizzaToppings.push($(this).val());
     });
 
-    console.log(newPizzaToppings);
-
-    addPizzatoOrder(newPizzaSize, newPizzaCroust, newPizzaToppings);
+    addPizzatoOrder(newPizzaSize, newpizzaCrust, newPizzaToppings);
 
     $("#checkout").show();
     $("#add-pizza").hide();
     $("#add-more-pizza").show();
     $("ul#pizza-order").append("<li class='list-group-item list-title'>Your custom Pizza:</li>");
     $("ul#pizza-order").append("<li class='list-group-item'>Your Size: " + newPizzaOrder.pizzaSize + ".</li>");
-    $("ul#pizza-order").append("<li class='list-group-item'>Your Croust: " + newPizzaOrder.pizzaCroust + ".</li>");
+    $("ul#pizza-order").append("<li class='list-group-item'>Your crust: " + newPizzaOrder.pizzaCrust + ".</li>");
     $("ul#pizza-order").append("<li class='list-group-item'><strong>Your Toppings:</strong></li>");
     newPizzaOrder.pizzaToppings.forEach(function(toppings){
     $("ul#pizza-order").append("<li class='list-group-item'>" + toppings + ".</li>");
--- readme.md
+++ readme.md
@@ -4,9 +4,16 @@ This is a starter pack for Epicodus Intro to Programming Projects.
 
 ## Specs
 
-| Behavior | Input | Output |
+| Behavior | Example Input | Example Output |
 |---|---|---|
-|User
+| User selects a pizza size, price increases accordingly | Large | Increase the price by $10 |
+| User selects a pizza crust, price increases accordingly | Crispy | Increase the price by $5 |
+| User selects a topping, price increases accordingly | Tomatoes | Increase the price by $1 |
+| User selects several toppings, price increases accordingly | Tomatoes, Cheese | Increase the price by $2 |
+| User wants to add another pizza, the form empties | Click "add another pizza"| Empty form values|
+| User wants to checkout, should be able to see the order summary and select delivery or carry out | Click "ckeckout" | Display order summary and delivery options |
+| User wants delivery, should be able to input address | Click "delivery"| Display address form |
+| User wants pickup, should see a greeting message | Click "carryout" | "Thanks! You can pick up your order at your nearest store." |
 
 ## Credits
 
